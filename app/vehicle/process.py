edd5f2be9ddb604cdf5749b2c4709d90ceaed41d
Diffeomorphisme <nicolas.maxant@queenslab.se>
2024-09-08 22:02:03 +0200

    feat(api): adding logging

--- app/vehicle/process.py
+++ app/vehicle/process.py
@@ -1,3 +1,4 @@
+from app import logger
 from app.vehicle.model import VehiclePassage
 from app.core.repository.repository import vehicle_repository
 
@@ -7,6 +8,8 @@ def process_vehicle_toll_information(vehicle_passage_data: VehiclePassage):
         vehicle_passage_data.vehicle_number
     )
     if existing_vehicle:
+        logger.info("Existing vehicle, updating activity")
         vehicle_repository.update(vehicle_passage_data)
     else:
+        logger.info("New vehicle, creating record")
         vehicle_repository.add(vehicle_passage_data)
